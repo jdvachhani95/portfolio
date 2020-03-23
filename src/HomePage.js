906cdb0516147544bdd27ba2ce9ca7449607b12c
jdvachhani95 <jaydeepvachhani22@gmail.com>
2020-03-22 22:31:30 -0400

    adjust background image on home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,40 +1,39 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import Background from "./assests/homeImage.jpg";
-import "typeface-roboto";
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import Background from './assests/homeImage.jpg';
+import 'typeface-roboto';
 import './HomePage.css';
-import Skills from "./Skills";
-
+import Skills from './Skills';
 
 const useStyles = makeStyles(theme => ({
-
   homeContent: {
-    fontFamily: "Helvetica Neue",
-    fontStyle: "normal",
-    fontSize: "40px",
-    color: "white",
-    margin: "auto",
-    textAlign: "center"
+    fontFamily: 'Helvetica Neue',
+    fontStyle: 'normal',
+    fontSize: '40px',
+    color: 'white',
+    margin: 'auto',
+    textAlign: 'center',
   },
   typo: {
-    textAlign: "center",
-    color: "#E31B6D"
+    textAlign: 'center',
+    color: '#E31B6D',
   },
-  
+
   homeImage: {
-    height: "100vh",
+    height: '100vh',
     width: '100%',
-    textAlign: "center",
+    textAlign: 'center',
     backgroundImage: `url(${Background})`,
-    backgroundRepeat: "repeat",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
     // margin: "0px",
-    padding: '0px'
+    padding: '0px',
   },
 }));
 
@@ -42,33 +41,41 @@ const HomePage = props => {
   const classes = useStyles();
 
   return (
-    
     <div>
       <Grid item xs={12}>
-      <section className={classes.homeImage}>
-      <div>
-        <p className={classes.homeContent}>
-          Hello I'm 
-          <span className={classes.typo}>Aruni Patel</span>.
+        <section className={classes.homeImage}>
+          <div>
+            <p className={classes.homeContent}>
+              Hello I'm
+              <span className={classes.typo}>Aruni Patel</span>.
+            </p>
+            <p className={classes.homeContent}>I'm Web Developer.</p>
 
-        </p>
-        <p className={classes.homeContent}>
-        I'm Web Developer.
-        </p>
-        
-        <div className="homeWork" style={{"&:hover":{backgroundColor:"#555"}}}>
-          View my work
-          <ArrowForwardIcon className="Arrow" style={{position: "absolute", color: "white", fontSize: "30", right:"25px" }} />
-        </div>
+            <div
+              className="homeWork"
+              style={{ '&:hover': { backgroundColor: '#555' } }}
+            >
+              View my work
+              <ArrowForwardIcon
+                className="Arrow"
+                style={{
+                  position: 'absolute',
+                  color: 'white',
+                  fontSize: '30',
+                  right: '25px',
+                }}
+              />
+            </div>
+          </div>
+        </section>
+      </Grid>
+
+      {/* <div><NewNavbar /></div> */}
+      <div>
+        <Skills />
       </div>
-    </section>
-    </Grid>
-    
-    {/* <div><NewNavbar /></div> */}
-    <div><Skills /></div>
     </div>
   );
-  
 };
 
 export default HomePage;
