a7057e5812f7e35b2a5595541b5ec3b7d4fb9546
jdvachhani95 <jaydeepvachhani22@gmail.com>
2020-04-14 17:33:08 -0400

    Add new navbar

--- src/NewNavbar.js
+++ src/NewNavbar.js
@@ -1,58 +1,63 @@
-import React, { Fragment } from "react";
-import { Button} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import { Link } from "react-router-dom";
+import React, { Fragment } from 'react';
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
   navbar: {
-    width: "100%",
-    height: "50px",
-    position: "absolute",
+    width: '100%',
+    height: '50px',
+    position: 'absolute',
     left: 0,
     right: 0,
-    borderBottom: "4px solid #04c2c9",
+    borderBottom: '4px solid #04c2c9',
     boxShadow: '4px 4px 10px 0 rgba(0, 0, 0, 0.3)',
-    textAlign: "left",
-    transition: "0.5s",
-    cursor: "pointer",
-    zIndex: 99
+    textAlign: 'left',
+    transition: '0.5s',
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'flexEnd',
+    zIndex: 99,
+    backgroundColor: '#1b242f',
   },
 
   wContainer: {
-    height: "100%",
-    margin: "0 auto",
-    "&:after": {
-      content: "",
-      display: "block",
-      clear: "both"
+    height: '100%',
+    margin: '0 auto',
+    alignItems: 'right',
+    '&:after': {
+      content: '',
+      display: 'block',
+      clear: 'both',
     },
-    backgroundColor: "#1b242f"
+    backgroundColor: '#1b242f',
   },
 
   button: {
-    transition: "0.5s",
-    fontFamily: "Raleway",
-    color: "white",
-    fontSize: "20px",
-    margin: "0px 20px",
-    "&:hover": {
-      color: "#e31b6d"
-    }
+    transition: '0.5s',
+    fontFamily:
+      'brandon-grotesque,HelveticaNeue-Light,Helvetica Neue Light,Helvetica Neue,Helvetica,Arial,Lucida Grande,sans-serif',
+    color: 'white',
+    fontSize: '20px',
+    margin: '0px 20px',
+    '&:hover': {
+      color: '#e31b6d',
+    },
   },
 
   buttonContainer: {
-    height: "50px",
-    float: "left",
-    justifyContent: "center",
-    alignItems: "center"
+    height: '50px',
+    float: 'left',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
   center: {
-    height: "50px",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    height: '50px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 }));
 
 const NewNavbar = props => {
@@ -60,12 +65,15 @@ const NewNavbar = props => {
   return (
     <Fragment>
       <div className={classes.navbar}>
-
         <div className={classes.wContainer}>
           <div className={classes.buttonContainer}>
-
-            <Button className={classes.button} style={{marginLeft: '80px'}} component={Link} to="/">
-              HOME
+            <Button
+              className={classes.button}
+              style={{ marginLeft: '80px' }}
+              component={Link}
+              to="/"
+            >
+              Home
             </Button>
 
             <Button className={classes.button} component={Link} to="/skills">
@@ -73,14 +81,16 @@ const NewNavbar = props => {
             </Button>
 
             <Button className={classes.button} component={Link} to="/about">
-              PROJECTS
+              Projects
             </Button>
 
             <Button
-              className={classes.button} component={Link} to="/contact/514">
-              CONTACT
+              className={classes.button}
+              component={Link}
+              to="/contact/514"
+            >
+              Contact
             </Button>
-
           </div>
         </div>
       </div>
