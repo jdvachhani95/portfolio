58323c308747e33da5a2e6b42b1fc77d65e82816
jdvachhani95 <jaydeepvachhani22@gmail.com>
2020-04-17 14:59:20 -0400

    Fix grid layout

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -33,21 +33,28 @@ const useStyles = makeStyles((theme) => ({
   },
   title1: {
     color: '#5e98dc',
-    textAlign: 'center',
     fontFamily: '"Mada", sans-serif',
-    fontSize: '36px',
-    lineHeight: '54px',
     fontWeight: 'normal',
-    marginTop: theme.spacing(2),
+    fontSize: '24px',
+    lineHeight: '35px',
+    [theme.breakpoints.up('lg')]: {
+      fontSize: '36px',
+      lineHeight: '54px',
+    },
   },
   title2: {
     color: '#5e98dc',
     textAlign: 'center',
     fontFamily: '"Mada", sans-serif',
-    fontSize: '65px',
-    lineHeight: '84px',
-    fontWeight: 600,
-    marginTop: theme.spacing(2),
+    fontSize: '50px',
+    lineHeight: '65px',
+    fontWeight: 'normal',
+    marginTop: '40px',
+    [theme.breakpoints.up('lg')]: {
+      fontSize: '65px',
+      lineHeight: '84px',
+      marginTop: '80px',
+    },
   },
 
   content: {
@@ -55,109 +62,140 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 'normal',
     fontStyle: 'normal',
     color: '#4f4f4f',
-    textAlign: 'center',
-    fontSize: '20px',
+    fontSize: '19px',
     lineHeight: '30px',
-    // margin: '0px 0px 20px',
   },
   gridBody: {
-    padding: '93px 35px',
+    padding: '50px 35px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    [theme.breakpoints.up('md')]: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      padding: '90px 75px',
+    },
   },
-  gridImg: {
-    // width: '0%',
+  middleGridBody: {
+    padding: '50px 35px',
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    justifyContent: 'center',
+    [theme.breakpoints.up('md')]: {
+      flexDirection: 'row',
+      padding: '90px 75px',
+      justifyContent: 'center',
+    },
+  },
+  leftGridImg: {
     borderRadius: '35px',
-    // height: '120%',
+    maxWidth: '310px',
+    maxHeight: '180px',
+    [theme.breakpoints.up('sm')]: {
+      maxWidth: '630px',
+      maxHeight: '330px',
+      margin: '0px auto',
+    },
+    [theme.breakpoints.up('md')]: {
+      maxWidth: '500px',
+      maxHeight: '280px',
+      margin: '0px 50px 0px 0px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      maxWidth: '730px',
+      maxHeight: '430px',
+      margin: '0px 100px 0px 0px',
+    },
+  },
+  rightGridImg: {
+    borderRadius: '35px',
+    maxWidth: '310px',
+    maxHeight: '180px',
+    [theme.breakpoints.up('sm')]: {
+      maxWidth: '630px',
+      maxHeight: '330px',
+      margin: '0px auto',
+    },
+    [theme.breakpoints.up('md')]: {
+      maxWidth: '500px',
+      maxHeight: '280px',
+      margin: '0px 0px 0px 50px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      maxWidth: '730px',
+      maxHeight: '430px',
+      margin: '0px 0px 0px 100px',
+    },
+  },
+  gridContentBody: {
+    margin: '20px auto 0px',
+    maxWidth: '500px',
+    textAlign: 'center',
+    [theme.breakpoints.up('md')]: {
+      textAlign: 'left',
+      margin: '20px 0px 0px 0px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      margin: '50px 0px 0px 0px',
+    },
   },
 }));
 
 const AboutMe = (props) => {
   const classes = useStyles();
-
+  // const matches = useMediaQuery('(min-width:600px)');
   const body1 = (
-    <section>
-      <Typography variant="h3" className={classes.title2}>
-        What I do
-      </Typography>
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography variant="h3" className={classes.title2}>
+          What I can do.
+        </Typography>
+        <div className={classes.gridBody}>
+          <img className={classes.leftGridImg} alt="img1" src={aboutMe1} />
+          <div className={classes.gridContentBody}>
+            <Typography variant="h2" className={classes.title1}>
+              Develop what you need
+            </Typography>
+            <Typography variant="h6" className={classes.content}>
+              Host and maintain the web application on cloud plateform like AWS,
+              Google cloud plateform. <br />
+              Handle client side requests using graphQL or RESTful webservices.
+              <br /> Handle SQL or NoSQL database.
+            </Typography>
+          </div>
+        </div>
 
-      {/* First */}
-      <Grid container className={classes.gridBody}>
-        <Grid
-          item
-          xs={12}
-          md={7}
-          style={{ display: 'flex', justifyContent: 'flex-end' }}
-        >
-          <img
-            className={classes.gridImg}
-            alt="img1"
-            src={aboutMe1}
-            style={{ marginRight: '50px' }}
-          />
-        </Grid>
-        <Grid item xs={12} md={5}>
-          <Typography
-            variant="h2"
-            className={classes.title1}
-            style={{ textAlign: 'left', marginLeft: '50px' }}
-          >
-            Hi
-          </Typography>
-          <Typography
-            variant="h6"
-            className={classes.content}
-            style={{ textAlign: 'left', marginLeft: '50px' }}
-          >
-            I'm a full stack developer, living in Toronto, Canada. I have a
-            passion for web developement & programming. I have 2+ years
-            experience in full stak developement.
-          </Typography>
-        </Grid>
-      </Grid>
+        {/* Second */}
+        <div className={classes.middleGridBody}>
+          <div className={classes.gridContentBody}>
+            <Typography variant="h2" className={classes.title1}>
+              Develop Front-end
+            </Typography>
+            <Typography variant="h6" className={classes.content}>
+              Build front-end with React using material-ui lib & javascript
+              frameworks. <br />
+              Conceive responsive design which support all type of devices and
+              offers cross-browser compatibility.
+            </Typography>
+          </div>
+          <img className={classes.rightGridImg} alt="img1" src={aboutMe2} />
+        </div>
 
-      {/* Second */}
-      <Grid container className={classes.gridBody}>
-        <Grid item xs={12} md={5}>
-          <Typography variant="h2" className={classes.title1}>
-            Hi
-          </Typography>
-          <Typography variant="h6" className={classes.content}>
-            I'm a full stack developer, living in Toronto, Canada. I have a
-            passion for web developement & programming. I have 2+ years
-            experience in full stak developement.
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={7}
-          style={{ display: 'flex', justifyContent: 'flex-start' }}
-        >
-          <img className={classes.gridImg} alt="img1" src={aboutMe2} />
-        </Grid>
+        {/* Third */}
+        <div className={classes.gridBody}>
+          <img className={classes.leftGridImg} alt="img1" src={aboutMe3} />
+          <div className={classes.gridContentBody}>
+            <Typography variant="h2" className={classes.title1}>
+              Work as a team player
+            </Typography>
+            <Typography variant="h6" className={classes.content}>
+              Communicate effectively with team using github and slack <br />
+              Work in cross-functional team
+            </Typography>
+          </div>
+        </div>
       </Grid>
-
-      {/* Third */}
-      <Grid container className={classes.gridBody}>
-        <Grid
-          item
-          xs={12}
-          md={7}
-          style={{ display: 'flex', justifyContent: 'flex-end' }}
-        >
-          <img className={classes.gridImg} alt="img1" src={aboutMe3} />
-        </Grid>
-        <Grid item xs={12} md={5}>
-          <Typography variant="h2" className={classes.title1}>
-            Hi
-          </Typography>
-          <Typography variant="h6" className={classes.content}>
-            I'm a full stack developer, living in Toronto, Canada. I have a
-            passion for web developement & programming. I have 2+ years
-            experience in full stak developement.
-          </Typography>
-        </Grid>
-      </Grid>
-    </section>
+    </Grid>
   );
 
   return (
@@ -166,10 +204,18 @@ const AboutMe = (props) => {
         <Grid item xs={12}>
           <div className={classes.avatarBody}>
             <Avatar src={AvatarImage} className={classes.avatar} />
-            <Typography variant="h2" className={classes.title1}>
+            <Typography
+              variant="h2"
+              className={classes.title1}
+              style={{ textAlign: 'center', marginTop: '20px' }}
+            >
               Hi
             </Typography>
-            <Typography variant="h6" className={classes.content}>
+            <Typography
+              variant="h6"
+              className={classes.content}
+              style={{ textAlign: 'center' }}
+            >
               I'm a full stack developer, living in Toronto, Canada. I have a
               passion for web developement & programming. I have 2+ years
               experience in full stak developement.
