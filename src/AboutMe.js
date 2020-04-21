96524e7b1a367d53493b494f1d747a3d0917afd6
jdvachhani95 <jaydeepvachhani22@gmail.com>
2020-04-21 00:50:57 -0400

    extract sub components to seperate file

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import PieChart from './PieChart';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Grid, Avatar, Paper, Button } from '@material-ui/core';
-import { Element } from 'react-scroll';
+import { Typography, Grid, Avatar } from '@material-ui/core';
 
 //Images
 import AvatarImage from './assests/avatar.jpg';
 import aboutMe1 from './assests/aboutMe/aboutMe1.jpg';
 import aboutMe2 from './assests/aboutMe/aboutMe2.jpg';
 import aboutMe3 from './assests/aboutMe/aboutMe3.jpg';
-
-//Icons
-import mediumIcon from './assests/Icons/mediumIcon.png';
-import resumeIcon from './assests/Icons/resumeIcon.png';
-import projectIcon from './assests/Icons/projectIcon.png';
-import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import PublicIcon from '@material-ui/icons/Public';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,14 +21,6 @@ const useStyles = makeStyles((theme) => ({
     margin: '0px auto',
     marginTop: theme.spacing(15),
     maxWidth: '600px',
-  },
-  charts: {
-    color: '#1b242f',
-    textAlign: 'center',
-    fontFamily: '"Mada", sans-serif',
-    fontSize: '36px',
-    margin: '0px 0px 20px',
-    fontWeight: 600,
   },
   title1: {
     color: '#5e98dc',
@@ -148,40 +130,6 @@ const useStyles = makeStyles((theme) => ({
       margin: '50px 0px 0px 0px',
     },
   },
-  paper: {
-    height: 350,
-    width: 275,
-    // backgroundColor: '#e8e8ed',
-    textAlign: 'center',
-    padding: theme.spacing(5),
-  },
-  cardHeading: {
-    fontFamily: '"Mada", sans-serif',
-    fontStyle: 'normal',
-    fontWeight: 600,
-    fontSize: '24px',
-    lineHeight: '36px',
-    color: '#000000',
-    marginTop: '20px',
-  },
-  cardContent: {
-    fontFamily: 'sans-serif',
-    marginTop: '10px',
-    fontWeight: 'normal',
-    fontStyle: 'normal',
-    color: '#4f4f4f',
-    fontSize: '19px',
-    lineHeight: '30px',
-  },
-  cardButton: {
-    fontFamily: 'sans-serif',
-    margin: theme.spacing(1),
-    padding: '2px 27px',
-    textTransform: 'none',
-    fontSize: '19px',
-    borderRadius: '25px',
-    color: '#5e98dc',
-  },
 }));
 
 const AboutMe = (props) => {
@@ -267,125 +215,10 @@ const AboutMe = (props) => {
     </Grid>
   );
 
-  const body3 = (
-    <Grid
-      item
-      xs={12}
-      style={{
-        justifyContent: 'center',
-        maxWidth: '600px',
-        margin: '0px auto',
-      }}
-    >
-      <Typography variant="h3" className={classes.charts}>
-        Skills I' good at.
-      </Typography>
-      <Element id="skills-component" name="skills-component">
-        <PieChart />
-      </Element>
-    </Grid>
-  );
-
-  const body4 = (
-    <div>
-      <Grid
-        container
-        style={{
-          flexGrow: 1,
-          margin: '25px 0px',
-        }}
-        spacing={2}
-      >
-        <Grid item xs={12}>
-          <Typography
-            variant="h3"
-            className={classes.title2}
-            style={{ margin: '30px 0px' }}
-          >
-            Resources
-          </Typography>
-          <Grid container justify="center" spacing={4}>
-            <Grid key={4} item>
-              <Paper className={classes.paper}>
-                <img alt="resume" src={resumeIcon} />
-                <Typography varient="h5" className={classes.cardHeading}>
-                  Resume
-                </Typography>
-                <Typography variant="h6" className={classes.cardContent}>
-                  Get more information about my work experience & education.
-                </Typography>
-                <Button
-                  variant="outlined"
-                  href="#outlined-buttons"
-                  endIcon={<CloudDownloadIcon />}
-                  className={classes.cardButton}
-                  size="large"
-                >
-                  download
-                </Button>
-              </Paper>
-            </Grid>
-
-            <Grid key={4} item>
-              <Paper className={classes.paper}>
-                <img
-                  alt="medium"
-                  src={mediumIcon}
-                  className={classes.cardLogo}
-                />
-                <Typography varient="h5" className={classes.cardHeading}>
-                  Blogs
-                </Typography>
-                <Typography variant="h6" className={classes.cardContent}>
-                  Visit the blogs i have written on medium.com
-                </Typography>
-                <Button
-                  variant="outlined"
-                  href="#outlined-buttons"
-                  endIcon={<AssignmentIcon />}
-                  className={classes.cardButton}
-                  size="large"
-                >
-                  visit blog
-                </Button>
-              </Paper>
-            </Grid>
-            <Grid key={4} item>
-              <Paper className={classes.paper}>
-                <img
-                  alt="project"
-                  src={projectIcon}
-                  className={classes.cardLogo}
-                />
-                <Typography varient="h5" className={classes.cardHeading}>
-                  Projects
-                </Typography>
-                <Typography variant="h6" className={classes.cardContent}>
-                  Explore the projects i made so far.
-                </Typography>
-                <Button
-                  variant="outlined"
-                  href="#outlined-buttons"
-                  endIcon={<PublicIcon />}
-                  className={classes.cardButton}
-                  size="large"
-                >
-                  Website
-                </Button>
-              </Paper>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </div>
-  );
-
   return (
     <div className={classes.root}>
       {body1}
       {body2}
-      {body3}
-      {body4}
     </div>
   );
 };
