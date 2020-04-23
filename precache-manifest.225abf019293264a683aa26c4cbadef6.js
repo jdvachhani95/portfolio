04f1bc8626d9a81892920e7dad2e86d36a9da2e5
jdvachhani95 <jaydeepvachhani22@gmail.com>
2020-04-22 22:14:18 -0400

    Updates

---
+++ precache-manifest.225abf019293264a683aa26c4cbadef6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f82b1b5c009eaa78807f7f02cf9f4b3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "15b906c1933195c20bd3",
+    "url": "/portfolio/static/css/main.455927cf.chunk.css"
+  },
+  {
+    "revision": "9272e7ad984de8346103",
+    "url": "/portfolio/static/js/2.e80f68c5.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/portfolio/static/js/2.e80f68c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b906c1933195c20bd3",
+    "url": "/portfolio/static/js/main.bd330d0a.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "fd5f85f5698c331559f08fca478fedec",
+    "url": "/portfolio/static/media/aboutMe1.fd5f85f5.jpg"
+  },
+  {
+    "revision": "94819b4e0fb08db4e3f3451a61ea88c8",
+    "url": "/portfolio/static/media/aboutMe2.94819b4e.jpg"
+  },
+  {
+    "revision": "c7e6388ce008adcbba8547e907c369ce",
+    "url": "/portfolio/static/media/aboutMe3.c7e6388c.jpg"
+  },
+  {
+    "revision": "21490809fdd0c056297682c517dd92bc",
+    "url": "/portfolio/static/media/avatar.21490809.jpg"
+  },
+  {
+    "revision": "14f4e7f4c6d3bfc0ea7baf1e64692424",
+    "url": "/portfolio/static/media/contactForm.14f4e7f4.jpg"
+  },
+  {
+    "revision": "671c46f1c9d6635223c3d281f1bf2288",
+    "url": "/portfolio/static/media/homeImage.671c46f1.jpg"
+  }
+]);
